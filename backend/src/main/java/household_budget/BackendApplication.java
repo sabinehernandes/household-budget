8fa0546c265e334b4a8f4e3d4346cbc012a69df9
Sabine <sabinehernandes@gmail.com>
2024-11-09 17:35:57 +0100

    add initial springboot config

---
+++ backend/src/main/java/household_budget/BackendApplication.java
@@ -0,0 +1,13 @@
+package household_budget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/household_budget/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package household_budget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
