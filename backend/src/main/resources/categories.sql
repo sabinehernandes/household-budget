0327545346b06c9f1411355040e90bcac0e21c6c
Sabine <sabinehernandes@gmail.com>
2024-11-09 19:20:09 +0100

    refactor column names

--- backend/src/main/resources/categories.sql
+++ backend/src/main/resources/categories.sql
@@ -1,7 +1,7 @@
-INSERT INTO categories(id, name) VALUES('30f78b6d-3f01-4f96-ad0c-d87c723502f0', 'housing');
-INSERT INTO categories(id, name) VALUES('743bc661-ab13-4c1b-af90-5f11fe03db21', 'groceries');
-INSERT INTO categories(id, name) VALUES('3ed96650-d0ab-4cad-bf03-04ac8a4a0d76', 'restaurants');
-INSERT INTO categories(id, name) VALUES('1321ae68-e8f9-4491-9f5a-3d226d4ffaf5', 'clothing');
-INSERT INTO categories(id, name) VALUES('cc175048-2007-4e22-b0aa-722fa950cc91', 'healthcare');
-INSERT INTO categories(id, name) VALUES('59820460-f1cb-4579-b12f-2ab963e8503b', 'subscriptions');
-INSERT INTO categories(id, name) VALUES('ffe42758-9274-4060-9056-046b2aa4fa38', 'transport');
+INSERT INTO categories(category_id, category_name) VALUES('30f78b6d-3f01-4f96-ad0c-d87c723502f0', 'housing');
+INSERT INTO categories(category_id, category_name) VALUES('743bc661-ab13-4c1b-af90-5f11fe03db21', 'groceries');
+INSERT INTO categories(category_id, category_name) VALUES('3ed96650-d0ab-4cad-bf03-04ac8a4a0d76', 'restaurants');
+INSERT INTO categories(category_id, category_name) VALUES('1321ae68-e8f9-4491-9f5a-3d226d4ffaf5', 'clothing');
+INSERT INTO categories(category_id, category_name) VALUES('cc175048-2007-4e22-b0aa-722fa950cc91', 'healthcare');
+INSERT INTO categories(category_id, category_name) VALUES('59820460-f1cb-4579-b12f-2ab963e8503b', 'subscriptions');
+INSERT INTO categories(category_id, category_name) VALUES('ffe42758-9274-4060-9056-046b2aa4fa38', 'transport');
