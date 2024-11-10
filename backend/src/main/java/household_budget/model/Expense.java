2690cb023124909d6a56b97bdbd5b84f26a18481
Sabine <sabinehernandes@gmail.com>
2024-11-10 12:14:49 +0100

    create Expense entity class and boilerplate code

---
+++ backend/src/main/java/household_budget/model/Expense.java
@@ -0,0 +1,105 @@
+package household_budget.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "expenses")
+public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "expenses_id")
+    private String id;
+
+    private String createdAt;
+
+    private String paidBy;
+
+    @OneToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    @NotNull
+    private Category category;
+
+    private String description;
+
+    private int amount;
+
+    public Expense(String id, String createdAt, String paidBy, Category category, String description, int amount) {
+        this.id = id;
+        this.createdAt = createdAt;
+        this.paidBy = paidBy;
+        this.category = category;
+        this.description = description;
+        this.amount = amount;
+    }
+
+    public Expense() {
+    }
+
+    public void addExpense(String createdAt, String paidBy, Category category, String description, int amount) {
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getPaidBy() {
+        return paidBy;
+    }
+
+    public void setPaidBy(String paidBy) {
+        this.paidBy = paidBy;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Expense expense)) return false;
+        return amount == expense.amount && Objects.equals(id, expense.id) && Objects.equals(createdAt, expense.createdAt) && Objects.equals(paidBy, expense.paidBy) && Objects.equals(category, expense.category) && Objects.equals(description, expense.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, createdAt, paidBy, category, description, amount);
+    }
+}
