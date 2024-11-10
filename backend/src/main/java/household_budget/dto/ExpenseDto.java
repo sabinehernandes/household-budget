e54851a306c2a708073c98a7b27c35dffa4f9b38
Sabine <sabinehernandes@gmail.com>
2024-11-10 12:25:29 +0100

    add id to ExpenseDto return

--- backend/src/main/java/household_budget/dto/ExpenseDto.java
+++ backend/src/main/java/household_budget/dto/ExpenseDto.java
@@ -2,9 +2,10 @@ package household_budget.dto;
 
 import household_budget.model.Expense;
 
-public record ExpenseDto(String createdAt, String paidBy, String categoryName, String description, int amount) {
+public record ExpenseDto(String id, String createdAt, String paidBy, String categoryName, String description, int amount) {
     public static ExpenseDto fromEntity(Expense expense) {
         return new ExpenseDto(
+                expense.getId(),
                 expense.getCreatedAt(),
                 expense.getPaidBy(),
                 expense.getCategory().getCategoryName(),
