9f5647bd391a5a9aae61df09ccde654599ce7fff
Sabine <sabinehernandes@gmail.com>
2024-11-10 12:13:54 +0100

    add get method

--- backend/src/main/java/household_budget/controller/Controller.java
+++ backend/src/main/java/household_budget/controller/Controller.java
@@ -6,9 +6,12 @@ import household_budget.service.ExpenseService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class Controller {
@@ -16,6 +19,12 @@ public class Controller {
 
     public Controller(ExpenseService service) {
         this.service = service;
+    }
+
+    @GetMapping("/api/expenses/")
+    public ResponseEntity<List<ExpenseDto>> getAllExpenses() {
+        List<ExpenseDto> expenses = service.getAllExpenses();
+        return ResponseEntity.ok(expenses);
     }
 
     @PostMapping("/api/expenses/")
