4d71139886832c29eabaa1f2ab73b31b74c77ca7
Sabine <sabinehernandes@gmail.com>
2024-11-10 19:23:26 +0100

    add hadleDelete to table

--- frontend/src/charts/Table.tsx
+++ frontend/src/charts/Table.tsx
@@ -3,8 +3,10 @@ import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-quartz.css";
 import React, { useState } from "react";
+import { deleteExpense } from "../services/api";
 
 interface IRow {
+  id: string;
   createdAt: string;
   paidBy: string;
   categoryName: string;
@@ -14,9 +16,31 @@ interface IRow {
 
 interface TableProps {
   expenses: IRow[];
+  fetchExpenses: () => void;
 }
 
-export default function Table({ expenses }: TableProps) {
+export default function Table({ expenses, fetchExpenses }: TableProps) {
+  const [selectedRow, setSelectedRow] = useState<IRow | null>(null);
+
+  const handleRowClick = (row: IRow) => {
+    setSelectedRow(row);
+  };
+
+  const handleDelete = async () => {
+    if (selectedRow) {
+      try {
+        console.log("exp id ", selectedRow.id);
+        console.log("row", selectedRow);
+        await deleteExpense(selectedRow.id);
+        fetchExpenses();
+        setSelectedRow(null);
+      } catch (error) {
+        console.error("Failed: ", error);
+        alert("Error deleting expense. Try again later");
+      }
+    }
+  };
+
   const [colDefs] = useState<ColDef<IRow>[]>([
     { field: "createdAt" },
     { field: "paidBy" },
@@ -35,7 +59,13 @@ export default function Table({ expenses }: TableProps) {
         rowData={expenses}
         columnDefs={colDefs}
         defaultColDef={defaultColDef}
-      />
+        onRowClicked={(e) => handleRowClick(e.data)}
+      />{" "}
+      {selectedRow && (
+        <button onClick={handleDelete} className="delete-button">
+          Delete
+        </button>
+      )}
     </div>
   );
 }
