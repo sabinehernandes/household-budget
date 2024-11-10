c52778fadf799e1db018ae76fb8448885fe8115e
Sabine <sabinehernandes@gmail.com>
2024-11-10 19:22:55 +0100

    add fetchExpenses and handleSubmit. get user input from fields

--- frontend/src/BudgetForm.tsx
+++ frontend/src/BudgetForm.tsx
@@ -1,11 +1,63 @@
-export default function BudgetForm() {
+import { useEffect, useState } from "react";
+import { createExpense, getAllExpenses } from "./services/api";
+import ExpensesList from "./Expenses";
+import Table from "./charts/Table";
+
+export default function BudgetForm({ onSubmit }) {
+  const [expenses, setExpenses] = useState([]);
+  const [date, setDate] = useState("");
+  const [paidBy, setPaidBy] = useState("");
+  const [categoryName, setCategoryName] = useState("");
+  const [description, setDescription] = useState("");
+  const [amount, setAmount] = useState<number | "">("");
+
+  const fetchExpenses = async () => {
+    const data = await getAllExpenses();
+    setExpenses(data);
+  };
+
+  useEffect(() => {
+    fetchExpenses();
+  }, []);
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (!date || !paidBy || !categoryName || !amount) {
+      alert("Please fill in all required fields");
+      return;
+    }
+
+    const expenseData = {
+      createdAt: date,
+      paidBy,
+      categoryName,
+      description,
+      amount: Number(amount),
+    };
+
+    try {
+      const newExpense = await createExpense(expenseData);
+      if (newExpense) {
+        fetchExpenses();
+      }
+      setDate("");
+      setPaidBy("");
+      setCategoryName("");
+      setDescription("");
+      setAmount("");
+    } catch (error) {
+      alert("Error creating expense. Try again later.");
+    }
+  };
+
   return (
     <>
       <section className="budget-form bg-slate-100 m-10 p-10 rounded-md leading-relaxed">
         <h1 className="text-5xl text-gray-700 font-bold tracking-wide text-center">
           Household Budget
         </h1>
-        <form className="text-gray-700 text-xl my-8">
+        <form className="text-gray-700 text-xl my-8" onSubmit={handleSubmit}>
           <fieldset className="my-4">
             <label htmlFor="date" className="font-medium">
               Date
@@ -13,8 +65,10 @@ export default function BudgetForm() {
             <div className="mt-2">
               <input
                 type="date"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
                 className="shadow-sm ring-1 ring-inset ring-gray-300 p-1.5 text-lg rounded-sm"
-              />{" "}
+              />
             </div>
           </fieldset>
           <fieldset className="mb-4">
@@ -22,13 +76,27 @@ export default function BudgetForm() {
               Paid by
             </label>
             <div className="mt-2">
-              <input type="radio" name="person" id="sabine" />
+              <input
+                type="radio"
+                name="person"
+                id="sabine"
+                value="Sabine"
+                onChange={(e) => setPaidBy(e.target.value)}
+                checked={paidBy === "Sabine"}
+              />
               <label htmlFor="sabine" className="ml-3">
                 Sabine
               </label>
             </div>
             <div>
-              <input type="radio" name="person" id="sabine" />
+              <input
+                type="radio"
+                name="person"
+                id="beatriz"
+                value="Beatriz"
+                onChange={(e) => setPaidBy(e.target.value)}
+                checked={paidBy === "Beatriz"}
+              />
               <label htmlFor="beatriz" className="ml-3">
                 Beatriz
               </label>
@@ -39,17 +107,27 @@ export default function BudgetForm() {
             <select
               name="category"
               id="category"
+              value={categoryName}
+              onChange={(e) => setCategoryName(e.target.value)}
               className="shadow-sm ring-1 ring-inset ring-gray-300 p-1.5 text-lg rounded-sm mt-2"
             >
               <option value="select">--select an option--</option>
-              <option value="housing">Housing</option>
-              <option value="groceries">Groceries</option>
-              <option value="restaurant">Restaurant</option>
-              <option value="clothing">Clothing</option>
-              <option value="healthcare">Healthcare</option>
-              <option value="subscriptions">Subscriptions</option>
-              <option value="transport">Transport</option>
+              <option value="Housing">Housing</option>
+              <option value="Groceries">Groceries</option>
+              <option value="Restaurants">Restaurants</option>
+              <option value="Clothing">Clothing</option>
+              <option value="Healthcare">Healthcare</option>
+              <option value="Subscriptions">Subscriptions</option>
+              <option value="Transport">Transport</option>
             </select>
+          </fieldset>
+          <fieldset className="mb-9">
+            <p className="font-medium">Description</p>
+            <textarea
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              className="shadow-sm ring-1 ring-inset ring-gray-300 p-1.5 text-lg rounded-sm mt-2"
+            ></textarea>
           </fieldset>
           <fieldset className="mb-9">
             <p className="font-medium">Amount (SEK)</p>
@@ -57,6 +135,10 @@ export default function BudgetForm() {
               type="number"
               name="amount"
               id="amount"
+              value={amount}
+              onChange={(e) =>
+                setAmount(e.target.value ? Number(e.target.value) : "")
+              }
               className="shadow-sm ring-1 ring-inset ring-gray-300 p-1.5 text-lg rounded-sm mt-2"
             />
           </fieldset>
@@ -68,6 +150,7 @@ export default function BudgetForm() {
           </button>
         </form>
       </section>
+      <Table expenses={expenses} fetchExpenses={fetchExpenses} />
     </>
   );
 }
