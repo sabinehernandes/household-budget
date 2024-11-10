da236c8bcea4e90acc1d286b952623f35d48968e
Sabine <sabinehernandes@gmail.com>
2024-11-10 19:20:07 +0100

    add fetchExpenses and handleNewExpense

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,32 @@
+import { useEffect, useState } from "react";
 import "../css/index.css";
 import BudgetForm from "./BudgetForm";
-import Table from "./charts/Table";
+import Expenses from "./Expenses";
+import { createExpense, getAllExpenses } from "./services/api";
+import { Expense } from "./types";
 
 export default function App() {
+  const [expenses, setExpenses] = useState([]);
+
+  const fetchExpenses = async () => {
+    const data = await getAllExpenses();
+    setExpenses(data);
+  };
+
+  useEffect(() => {
+    fetchExpenses();
+  }, []);
+
+  const handleNewExpense = async (expenseData: Expense) => {
+    await createExpense(expenseData);
+    await fetchExpenses();
+    const data = await getAllExpenses();
+    setExpenses(data);
+  };
+
   return (
     <>
-      <BudgetForm />
-      <Table />
+      <BudgetForm onSubmit={handleNewExpense} />
     </>
   );
 }
