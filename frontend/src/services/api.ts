971517e1b2fd2518c3603029246faaef0870b597
Sabine <sabinehernandes@gmail.com>
2024-11-10 19:19:11 +0100

    add createExpense and deleteExpense methods

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,3 +1,5 @@
+import { Expense } from "../types";
+
 const URL = "http://localhost:3000/api/expenses";
 
 export async function getAllExpenses() {
@@ -8,5 +10,37 @@ export async function getAllExpenses() {
     return data;
   } catch (error) {
     console.error("Error fetching expenses: ", error);
+  }
+}
+
+export async function createExpense(expenseData: Expense) {
+  try {
+    const response = await fetch(URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(expenseData),
+    });
+    console.log("data: ", expenseData);
+
+    if (!response.ok) throw new Error("Failed to create expense");
+    return await response.json();
+  } catch (error) {
+    console.error("Error creating expense:", error);
+    throw error;
+  }
+}
+
+export async function deleteExpense(expenseId: string) {
+  try {
+    const response = await fetch(`${URL}/${expenseId}`, {
+      method: "DELETE",
+    });
+    if (!response.ok) throw new Error("Failed to delete expense");
+    return;
+  } catch (error) {
+    console.error("Error deleting expense:", error);
+    throw error;
   }
 }
